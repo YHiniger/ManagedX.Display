94a21b5e91bf9ae46e8cd54eb554a17b8669c79b
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2016-03-13 11:37:47 +0100

    Renamed DisplayFixedOutput to ModeScaling (more DXGI-friendly).

--- code/enumerations/ModeScaling.cs
+++ code/enumerations/ModeScaling.cs
@@ -4,15 +4,15 @@
 	/// <summary>Enumerates values specifying how the graphics device presents a low-resolution mode on a higher resolution monitor.
 	/// <para>This enumeration is equivalent to the native
 	/// <code>DMDFO_*</code> (Device Mode Display Fixed Output) constants (defined in WinGDI.h), and
-	/// the <code>DXGI_MODE_SCALING</code> enumeration (defined in DXGI.h).</para>
+	/// the <code>DXGI_MODE_SCALING</code> enumeration (defined in DXGIType.h).</para>
 	/// </summary>
 	/// <remarks>
-	/// https://msdn.microsoft.com/en-us/library/dd183565%28v=vs.85%29.aspx (DMDFO_*)
 	/// https://msdn.microsoft.com/en-us/library/windows/desktop/bb173066%28v=vs.85%29.aspx (DXGI_MODE_SCALING)
+	/// https://msdn.microsoft.com/en-us/library/dd183565%28v=vs.85%29.aspx (DMDFO_*)
 	/// </remarks>
+	[Design.Native( "DXGIType.h", "DXGI_MODE_SCALING" )]
 	[Design.Native( "WinGDI.h", "DMDFO_*" )]
-	[Design.Native( "DXGI.h", "DXGI_MODE_SCALING" )]
-	public enum DisplayFixedOutput : int
+	public enum ModeScaling : int
 	{
 		
 		/// <summary>Either the display's default setting, or unspecified.</summary>
